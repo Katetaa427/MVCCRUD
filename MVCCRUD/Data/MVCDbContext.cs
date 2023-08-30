49e91f6d0b34be399faa019c68e2c65f5dea369e
kate <katetaa427@gmail.com>
2023-08-30 11:41:32 -0700

    Add project files.

---
+++ MVCCRUD/Data/MVCDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCCRUD.Models.Domain;
+
+namespace MVCCRUD.Data
+{
+    public class MVCDbContext : DbContext
+    {
+        public MVCDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
